bf0a6750167c5ae2401cc3215f2a1746dc14fd58
martine701 <allietmartine@gmail.com>
2023-07-22 01:03:17 +0100

    sat commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren } from '@angular/core';
+
+@Component({
+  selector: 'Person',
+  template: `
+    <div>&nbsp;-&nbsp;<ng-content></ng-content></div>
+  `,
+  styles: ['']
+})
+export class Person { }
+
+@Component({
+  selector: 'Paragraph',
+  template: `<div>
+    <ng-content></ng-content>
+    <p *ngIf="people">Number of people:{{people.length}}</p>
+    </div>`,
+  styles: ['div{border:1px solid #c0c0c0;margin:10px;padding:10px}', 'p{margin:5px 0}']
+})
+export class Paragraph {
+  @ContentChildren(Person) people: any;
+}
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
+  <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    <Person>Albertus Falx</Person>
+    <Person>Godefridus Turpilius</Person>
+    <Person>Demipho Renatus</Person>
+  </Paragraph>
+  <Paragraph>Praesent eget ornare neque, vel consectetur eros.
+    <Person>Hanno Grumio</Person>
+    <Person>Lycus Auxilius</Person>
+  </Paragraph>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ch14-ex1000';
+  title = 'app works';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, Paragraph, Person } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, Paragraph, Person
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
